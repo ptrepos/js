93b5de322254602b1cca547e27936a8800d43e3a
takuo hada <t_hada@dachicraft.net>
2017-03-05 00:24:12 +0900

    テストパターン追加

--- plugin/magica/test.js
+++ plugin/magica/test.js
@@ -82,35 +82,113 @@ if(this.mg == undefined) {
 	html.pre = function(attrs, bodies) {
 		return html_element("pre", attrs, bodies);
 	};
+	html.dl = function(attrs, bodies) {
+		return html_element("dl", attrs, bodies);
+	};
+	html.dt = function(attrs, bodies) {
+		return html_element("dt", attrs, bodies);
+	};
+	html.dd = function(attrs, bodies) {
+		return html_element("dd", attrs, bodies);
+	};
 	
-	mg.AssertException = function(message) {
+	var _equals = function(a, b) 
+	{
+		return a === b;
+	};
+	
+	mg.AssertException = function(message)
+	{
 		this.message = message;
+		if(this.stack != null) {
+			this.stack = new Error().stack;
+		}
 	};
 	mg.AssertException.prototype = new Error("AssertException");
 
-	mg.assertEquals = function(expression, value, message) {
-		if(!(expression === value)) {
-			throw new mg.AssertException("excepted [" + value + "]");
+	mg.assertEquals = function(actual, excepted) 
+	{
+		if(!(_equals(actual, excepted))) {
+			throw new mg.AssertException("assertEquals [actual: " + actual.toString() + ", excepted: " + excepted.toString() +  "]");
 		}
-	}
-	mg.assertNotEquals = function(expression, value, message) {
-		if(!(expression !== value)) {
-			throw new mg.AssertException("excepted [" + value + "]");
+	};
+	mg.assertNotEquals = function(actual, excepted) 
+	{
+		if(!(!_equals(actual, excepted))) {
+			throw new mg.AssertException("assertNotEquals [actual: " + actual.toString() + ", excepted: " + excepted.toString() +  "]");
 		}
-	}
-	mg.assertNull = function(expression, message) {
-		if(!(expression === null)) {
-			throw new mg.AssertException("excepted");
+	};
+	mg.assertSame = function(actual, excepted) 
+	{
+		if(!(actual === excepted)) {
+			throw new mg.AssertException("assertSame [actual: " + actual.toString() + ", excepted: " + excepted.toString() +  "]");
 		}
-	}
-	mg.assertNotNull = function(expression, message) {
-		if(!(expression !== null)) {
-			throw new mg.AssertException("excepted");
+	};
+	mg.assertNotSame = function(actual, excepted) 
+	{
+		if(!(actual !== excepted)) {
+			throw new mg.AssertException("assertNotSame [actual: " + actual.toString() + ", excepted: " + excepted.toString() +  "]");
 		}
-	}
+	};
+	mg.assertNull = function(value) 
+	{
+		if(!(value === null)) {
+			throw new mg.AssertException("assertNull [value:" + value.toString() + "]");
+		}
+	};
+	mg.assertNotNull = function(value) 
+	{
+		if(!(value !== null)) {
+			throw new mg.AssertException("assertNotNull [value: null]");
+		}
+	};
+	mg.assertTrue = function(value) 
+	{
+		if(!(value === true)) {
+			throw new mg.AssertException("assertTrue [value:" + value.toString() + "]");
+		}
+	};
+	mg.assertFalse = function(value) 
+	{
+		if(!(value === false)) {
+			throw new mg.AssertException("assertFalse [value:" + value.toString() + "]");
+		}
+	};
+	mg.assertUndefined = function(value) 
+	{
+		if(!(value === undefined)) {
+			throw new mg.AssertException("assertUndefined [value:" + value.toString() + "]");
+		}
+	};
+	mg.assertArrayEquals = function(actual, excepted)
+	{
+		if(actual.length != excepted.length) {
+			throw new mg.AssertException("assertArrayEquals unmatch length [actual: " + actual.toString() + ", excepted: " + excepted.toString() + "]");
+		}
+		for(var i = 0; i < actual.length; i++) {
+			if(!(_equals(actual[i], excepted[i]))) {
+				throw new mg.AssertException("assertArrayEquals [actual: " + actual[i].toString() + ", excepted: " + excepted[i].toString() + "]");
+			}
+		}
+	};
+	mg.assertArrayNotEquals = function(actual, excepted)
+	{
+		if(actual.length != excepted.length) {
+			return;
+		}
+		var unmatch = false;
+		for(var i = 0; i < actual.length; i++) {
+			if(!(_equals(actual[i], excepted[i]))) {
+				unmatch = true;
+			}
+		}
+		if(!unmatch) {
+			throw new mg.AssertException("assertArrayNotEquals [actual: " + actual.toString() + ", excepted: " + excepted.toString() + "]");
+		}
+	};
 	mg.fail = function(message) {
-		throw new mg.AssertException(message);
-	}
+		throw new mg.AssertException(isSetted(message) ? message: "fail");
+	};
 	
 	mg.TestRunner = function() {
 		this._testCases = [];
@@ -119,37 +197,39 @@ if(this.mg == undefined) {
 		this._resultContext = null;
 		this._statusContext = null;
 		this._testCaseContext = null;
+		this._block = null;
+		this._showButton = null;
 	};
 	
 	var TestRunner_ok = function(thisObj, name)
 	{
 		thisObj._passes++;
 		
-		var block = html.div("mgBlock mgOK", [
-			html.div("mgHead", [
-				html.span("mgType", "[OK]"), " ",
+		var block = html.dl("mgBlock mgOK", [
+			html.dt("mgHead", [
+				html.span("mgType", "[OK]"), "\t",
 				html.span("mgName", name), " ",
 			])
 		]);
 		thisObj._testCaseContext.appendChild(block);
 		TestRunner_reportStatus(thisObj);
-	}
+	};
 
 	var TestRunner_error = function(thisObj, name, ex)
 	{
 		thisObj._errors++;
 		
-		var block = html.div("mgBlock mgError", [
-			html.div("mgHead", [
-				html.span("mgType", "[ERROR]"), " ",
-				html.span("mgName", name), " : ",
+		var block = html.dl("mgBlock mgError", [
+			html.dt("mgHead", [
+				html.span("mgType", "[ERROR]"), "\t",
+				html.span("mgName", name), " :\t",
 				html.span("mgMessage", ex.message),
 			]),
-			html.div("mgStack", html.pre(null, ex.stack)),
+			html.dd("mgStack", html.pre(null, ex.stack)),
 		]);
 		thisObj._testCaseContext.appendChild(block);
 		TestRunner_reportStatus(thisObj);
-	}
+	};
 	
 	var TestRunner_reportStatus = function(thisObj)
 	{
@@ -171,29 +251,29 @@ if(this.mg == undefined) {
 		
 		dom_removeChildren(thisObj._statusContext);
 		thisObj._statusContext.appendChild(block);
-	}
+	};
 
 	var TestRunner_log = function(thisObj, type, message)
 	{
 		thisObj._console.appendChild(html.span(type, message));
-	}
+	};
 
 	mg.out = function(message)
 	{
 		TestRunner_log(mg.testRunner, "mgText", message + "\r\n");
-	}
+	};
 	mg.error = function(message)
 	{
 		TestRunner_log(mg.testRunner, "mgError", message + "\r\n");
-	}
+	};
 	mg.warn = function(message)
 	{
 		TestRunner_log(mg.testRunner, "mgWarn", message + "\r\n");
-	}
+	};
 	mg.info = function(message)
 	{
 		TestRunner_log(mg.testRunner, "mgInfo", message + "\r\n");
-	}
+	};
 
 	mg.TestRunner.prototype.init = function()
 	{
@@ -207,8 +287,32 @@ if(this.mg == undefined) {
 			this._console,
 			this._resultContext
 		]);
+		this._block = block;
+
+		var showButton = html.div("mgTestRunnerShowButton", "TEST");
+		showButton.style.display = "none";
+		this._showButton = showButton;
+		
+		var blockClicked = false;
+		block.addEventListener("click", function() {
+			blockClicked = true;
+		});
+		showButton.addEventListener("click", function() {
+			blockClicked = true;
+			mg.testRunner.show();
+		});
+		window.addEventListener("click", function(e) {
+			if(e.button == 0) {
+				if(blockClicked == false) {
+					mg.testRunner.hide();
+				}
+			}
+			blockClicked = false;
+		});
+		
 		document.body.appendChild(block);
-	}
+		document.body.appendChild(showButton);
+	};
 	
 	mg.TestRunner.prototype.add = function(testCase)
 	{
@@ -220,7 +324,7 @@ if(this.mg == undefined) {
 				this._testCases.push({type: "test", name: name, test: testCase[name]});
 			}
 		}
-	}
+	};
 	
 	mg.TestRunner.prototype.run = function()
 	{
@@ -228,32 +332,45 @@ if(this.mg == undefined) {
 		var thisObj = this;
 		var testCases = thisObj._testCases;
 		var loop = function() {
-			if(index >= testCases.length) {
-				return;
-			}
-			if(testCases[index].type == "testCase") {
-				thisObj._testCaseContext = html.div("mgTestCaseContext", 
-					html.div("mgCaption", testCases[index].name));
-				thisObj._resultContext.appendChild(thisObj._testCaseContext);
-			} else {
-				var name = testCases[index].name;
-				var test = testCases[index].test;
-				
-				try {
-					test();
-					
-					TestRunner_ok(thisObj, name);
-				} catch(ex) {
-					TestRunner_error(thisObj, name, ex);
+			for(var i = 0; i < 8; i++) {
+				if(index >= testCases.length) {
+					return;
 				}
+				if(testCases[index].type == "testCase") {
+					thisObj._testCaseContext = html.div("mgTestCaseContext", 
+						html.div("mgCaption", testCases[index].name));
+					thisObj._resultContext.appendChild(thisObj._testCaseContext);
+				} else {
+					var name = testCases[index].name;
+					var test = testCases[index].test;
+					
+					try {
+						test();
+						
+						TestRunner_ok(thisObj, name);
+					} catch(ex) {
+						TestRunner_error(thisObj, name, ex);
+					}
+				}
+
+				index++;
 			}
-
-			index++;
-
 			setTimeout(loop, 0);
 		};
 		setTimeout(loop, 0);
-	}
+	};
+	
+	mg.TestRunner.prototype.hide = function()
+	{
+		this._block.style.display = "none";
+		this._showButton.style.display = "";
+	};
+	
+	mg.TestRunner.prototype.show = function()
+	{
+		this._block.style.display = "";
+		this._showButton.style.display = "none";
+	};
 	
 	mg.testRunner = new mg.TestRunner();
 	
